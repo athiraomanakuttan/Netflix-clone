eee97d517d991376770aca7181bfb2dee29fef86
athira <athiraomanakuttan20@gmail.com>
2024-10-01 10:22:52 +0530

    changed key to env

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,14 +3,15 @@ import { getFirestore , addDoc, collection} from "firebase/firestore";
 import {  toast } from 'react-toastify';
 
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword , getAuth, signOut} from 'firebase/auth'
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCMzmpMyVduyLg4wXsLzkJqtkLm9cvIDJk",
-  authDomain: "netflix-clone-6b889.firebaseapp.com",
-  projectId: "netflix-clone-6b889",
-  storageBucket: "netflix-clone-6b889.appspot.com",
-  messagingSenderId: "843204149862",
-  appId: "1:843204149862:web:9e700302a434ff73f8e3a7"
-};
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  };
 
 const app = initializeApp(firebaseConfig);
 
